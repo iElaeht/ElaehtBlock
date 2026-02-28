56ec2e0bf824c4a240326e93ceac663f3ac1834d
elaeht <elaeht2002@gmail.com>
2026-02-27 23:45:00 -0500

    Agregandolo a android apk

---
+++ android/app/src/main/java/com/elaeht/blockgame/MainActivity.java
@@ -0,0 +1,5 @@
+package com.elaeht.blockgame;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.elaeht.blockgame',
+  appName: 'elaehtBlock',
+  webDir: 'dist'
+};
+
+export default config;
